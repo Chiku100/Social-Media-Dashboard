74b0f3ae6effcdd04569d7ae6ad4d83d083d7e18
Chiku100 <104713816+Chiku100@users.noreply.github.com>
2022-09-27 20:52:31 +0530

    Updating logic

--- index.js
+++ index.js
@@ -1,68 +1,23 @@
-let containers = document.querySelectorAll(".container");
-let cons = $(".container")
-let spans = document.querySelectorAll("span");
-let smalls = document.querySelectorAll("small");
-let cards = document.querySelectorAll(".box1 p");
 $("button").click(function () {
     toggle();
 })
-
 function toggle() {
     if ($(".circle").css("right") == "2px") {
-        $("body").css("background-color", "hsl(230, 17%, 14%)");
-        document.querySelector("button").style.background = "linear-gradient(hsl(210, 78%, 56%), hsl(146,68%,55%))";
+        $("html").attr("data-theme", "dark");
+        $("button").css({ background: 'linear-gradient(hsl(210, 78%, 56%),hsl(146,68%,55%))' });
         $(".circle").css("right", "22px");
-        $("body").css("color", "white");
-        for (c of containers) {
-            c.style.backgroundColor = "hsl(228, 28%, 20%)";
-        }
-        for (s of spans) {
-            s.style.color = "hsl(228, 34%, 66%)";
-        }
-        $("label").css("color", "hsl(228, 34%, 66%)");
-        $("p").css("color", "hsl(228, 34%, 66%)");
-        $(".ovrv").css("color", "white");
-        $(".circle").css("background-color", "hsl(228, 28%, 20%)");
-        for (s of smalls) {
-            s.style.color = "hsl(228, 34%, 66%)";
-        }
-        for (c of cards) {
-            c.style.color = "hsl(228, 34%, 66%)";
-            for (let i = 0; i < containers.length; i++) {
-                containers[i].onmouseover = function () {
-                    containers[i].style.backgroundColor = "hsl(232, 19%, 25%)"
-                }
-                containers[i].onmouseout = function () {
-                    containers[i].style.backgroundColor = "hsl(228, 28%, 20%)"
-                }
-            }
-        }
+        animation();
+
     } else {
-        $("body").css("background-color", "white");
+        $("html").attr("data-theme", "light");
         $(".circle").css("right", "2px");
         $("button").css("background", "hsl(230, 22%, 74%)");
-        $("body").css("color", "black");
-        for (c of containers) {
-            c.style.backgroundColor = "hsl(225, 100%, 98%)";
-        }
-        for (s of spans) {
-            s.style.color = "hsl(228, 12%, 44%)";
-        }
-        for (s of smalls) {
-            s.style.color = "hsl(228, 12%, 44%)";
-        }
-        for (c of cards) {
-            c.style.color = "hsl(228, 12%, 44%)";
-        }
-        $(".circle").css("background-color", "white");
-        $(".ovrv").css("color", "hsl(228, 12%, 44%)");
-        for (let i = 0; i < containers.length; i++) {
-            containers[i].onmouseover = function () {
-                containers[i].style.backgroundColor = "hsl(227, 47%, 92%)"
-            }
-            containers[i].onmouseout = function () {
-                containers[i].style.backgroundColor = "hsl(225, 100%, 98%)"
-            }
-        }
+        animation();
     }
 }
+let animation = () => {
+    $("html").addClass("transition");
+    window.setTimeout(() => {
+        $("html").removeClass("transition");
+    }, 200);
+};
